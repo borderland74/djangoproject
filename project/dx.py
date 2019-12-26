3c57eb37166e347b8ba34ba63433ec58363ae021
shixiaojie <shixiaojie@aiyunxiao.com>
2019-12-26 22:54:12 +0800

    add: 11

--- project/dx.py
+++ project/dx.py
@@ -5,3 +5,4 @@ rand_str = random.randrange(1000,10000)
 __business_id = uuid.uuid1()
 params = "{\"code\":\"%s\"}"%rand_str
 send_sms(__business_id,"17633907126","捷豹快快","SMS_135740070",params)
+add1
